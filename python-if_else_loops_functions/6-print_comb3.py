2fe73900b1e6a3a51d7056680860fa1e17d93397
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-03 14:27:08 +0200

    modiication of task 5 and 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i != 0 or j != 1:
             print(", ", end="")
-        print(f"{i}{j}", end="")
+        print("{}{}" .format(i, j), end="")
 print()
