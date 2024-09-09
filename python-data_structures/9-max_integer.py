1e66137a312587ea977c38797ade2824f22157a3
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-09 14:00:03 +0200

    fix code

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 def max_integer(my_list=[]):
     if my_list == "":
         return None
-    max_integer = my_list[0]
-    for i in my_list:
-        if i > max_integer:
-            max_integer = i
-    return max_integer
+    else:
+        
+        max_integer = my_list[0]
+        for i in my_list:
+            if i > max_integer:
+                max_integer = i
+        return max_integer
