36be87aca829ec649c164b824f1febc1acc12ea5
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-12 13:08:12 +0200

    add description

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+"""Module for print_square function"""
+
+
+
 def print_square(size):
+    """
+    Print square with the character #.
+    
+    Args:
+    size: is the size of length of the square
+    
+    Returns:
+    Print square
+    
+    Raises:
+    TypeError:  if size < 0
+                if size is not an integer
+                
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""Module for text_indentation function"""
+
+
+
 def text_indentation(text):
+    """
+    Add new line after special characters
+    
+    Args:
+    text (str): is a text
+    char: list of char
+    
+    Returns:
+    print new line when function write special character
+    
+    Raises:
+    TypeError: if is not a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     result = ""
