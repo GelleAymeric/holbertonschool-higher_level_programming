fa117528c7b63af691e36edd1b48b8aa91f1667b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-12 13:41:45 +0200

    add code in task 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for text_indentation function"""
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
@@ -14,20 +15,18 @@ def text_indentation(text):
     Returns:
     None: This function prints the formatted text, it doesn't return anything
     """
-    # Vérifier si text est une chaîne
-    if type(text) != str:
-        raise TypeError("text must be a string")
 
-    # Parcourir chaque caractère du texte
-    for i in range(len(text)):
-        # Imprimer le caractère
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
         print(text[i], end="")
 
-        # Si le caractère est ., ? ou :, ajouter deux nouvelles lignes
         if text[i] in [".", "?", ":"]:
             print("\n")
 
-        # Si on a imprimé deux nouvelles lignes, ignorer les espaces suivants
-        if i < len(text) - 1 and text[i] in [".", "?", ":"] and text[i+1] == " ":
-            while i < len(text) - 1 and text[i+1] == " ":
+            i += 1
+            while i < len(text) and text[i] == " ":
                 i += 1
+            i -= 1
+        i += 1
