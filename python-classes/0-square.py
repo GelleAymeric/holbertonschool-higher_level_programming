270188314901f84b5e7be4859baeb2688a686c83
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-16 13:43:12 +0200

    add description

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Square():
+    """Represents a square with a specific size."""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,6 +6,7 @@ class Square:
     def __init__(self, size=0):
         """
         Initialize a Square.
+
         Args:
             size (int): The size of the square
         """
