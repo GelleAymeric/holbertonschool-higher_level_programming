dfd869efb12283c14222e370c85db803dfae3721
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-17 14:44:37 +0200

    fix problems

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -68,5 +68,5 @@ class Rectangle:
         Calcultate and return perimeter of rectangle
         """
         if self.__width == 0 or self.__height == 0:
-            print("")
+            return 0
         return (self.__width + self.__height) * 2
