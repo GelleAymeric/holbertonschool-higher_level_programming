d75c71196e0ce3639c6c8315a4af95535213d5ba
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-17 15:05:36 +0200

    fix and check code

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,7 +82,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         return "\n".join(
-            str(self.print_symbol * self.__width) for _ in range(self.__height)
+            str(self.print_symbol) * self.__width for _ in range(self.__height)
             )
 
     def __repr__(self):
