137425546ccac39d5690638ac88a3c421c468f83
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-17 15:04:27 +0200

    try to fix

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -85,7 +85,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         return "\n".join(
-            str("#" * self.__width) for _ in range(self.__height)
+        str(self.print_symbol) * self.__width for _ in range(self.__height)
         )
 
     def __repr__(self):
