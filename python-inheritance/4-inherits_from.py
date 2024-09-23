922e777c1c625439039bfad2aff4b2aa7e19d6a0
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-23 12:48:08 +0200

    try to check unitest

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
         a_class: The class to compare against.
 
     Returns:
-        bool: True if obj is an instance of 
+        bool: True if obj is an instance of
         a class that inherited from a_class, False otherwise.
     """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
