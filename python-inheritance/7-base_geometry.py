c68eb670917d9c9692bea65ca6051a561d530e05
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-23 12:46:06 +0200

    fix pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -25,6 +25,6 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0.
         """
         if not isinstance(value, int):
-                raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
