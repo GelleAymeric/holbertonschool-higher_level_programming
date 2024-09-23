52c67efe63a9b51dff7310206507f20588f96d4b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-23 10:30:49 +0200

    add files , readme and check task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list"""
+
+
+class MyList(list):
+    """method to print sorted list"""
+    def print_sorted(self):
+        """print the list"""
+        print(sorted(self))
