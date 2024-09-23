e747403557ecfaac4388b3df8aac12c4593c1487
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-23 11:09:04 +0200

    fix pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Define if the object is exactly an instance or not"""
 
+
 def is_same_class(obj, a_class):
     """
     Args:
         obj: object to check
         a_class: the class to compare
-    
+
     Return:
         True if is an instance , otherwise false
     """
