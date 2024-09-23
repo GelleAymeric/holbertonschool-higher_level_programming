23b3b5612b89bfcfd0b3e8764ee04f0ba2149e44
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-23 11:15:47 +0200

    check task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""function that returns if is an instance or not
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+      Args:
+        obj: object to check
+        a_class: the class to compare
+
+    Return:
+         True if the object is an instance of,
+        or if the object is an instance of a class that inherited from,
+        the specified class ; otherwise False.
+    """
+    if not isinstance(obj, a_class):
+        return True
+    else:
+        return False
