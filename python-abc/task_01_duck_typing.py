3307c9f1025aa33c813cadd291ff3b445052986c
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-24 14:53:01 +0200

    change problems

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -56,5 +56,5 @@ class Rectangle(Shape):
 
 def shape_info(shape):
     """Print the area and perimeter of the given shape."""
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
