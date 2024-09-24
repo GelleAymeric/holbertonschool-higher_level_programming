c4bfd1267abb15a15064c59233ddc7108446d240
Aymeric154 <aymeric.gelle@outlook.fr>
2024-09-24 14:07:44 +0200

    add task 4

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+class Fish:
+    @classmethod
+    def swim(self):
+        """Method to indicate fish swimming."""
+        print("The fish is swimming")
+    
+    @property
+    def habitat(self):
+        """Returns the habitat of the fish."""
+        return "The fish lives in water"
+        
+class Bird:
+    @classmethod
+    def fly(self):
+        """Method to indicate bird flying."""
+        print("The bird is flying")
+    
+    @property
+    def habitat(self):
+        """Returns the habitat of the bird."""
+        return "The bird lives in the sky"
+        
+class FlyingFish(Fish, Bird):
+    def swim(self):
+        """Method to indicate flying fish swimming."""
+        print("The flying fish is swimming")
+    
+    def fly(self):
+        """Method to indicate flying fish flying."""
+        print("The flying fish is flying")
+
+    @property
+    def habitat(self):
+        """Returns the habitat of the flying fish."""
+        return "The flying fish lives both in water and the sky!"
