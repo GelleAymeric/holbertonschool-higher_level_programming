05bef3a00c3acaf807c17eb67236ab6c87dec713
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-09 11:46:09 +0200

    find problem and fix it

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -22,11 +22,11 @@ def fetch_and_print_posts():
             )
 
 
-fetch_and_print_posts()
-
-
 def fetch_and_save_posts():
     """Fetch posts from the API and save them to a CSV file."""
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    
     if response.status_code == 200:
         posts = response.json()
 
@@ -38,13 +38,13 @@ def fetch_and_save_posts():
         for post in posts:
             print(post.get('body'))
 
-        with open("posts.csv", "w", newline='', encoding='utf-8') as csv_file:
+        with open("posts.csv", "w", newline='') as csv_file:
             writer = csv.writer(csv_file)
 
             writer.writerow(['id', 'title', 'body'])
 
             for post in data_posts:
-                writer.writerow([post['id'], post['title'], post['body']])
+                writer.writerow(post)
 
 
 fetch_and_save_posts()
