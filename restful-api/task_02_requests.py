3750c67b2b81cd6af58b3f276c4b9d155b76e9db
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-09 11:25:50 +0200

    add pycodestyle description

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,35 +6,45 @@ import csv
 url = "https://jsonplaceholder.typicode.com/posts"
 response = requests.get(url)
 
+
 def fetch_and_print_posts():
+    """Fetch posts from the API and print them."""
 
     print(f"Statut code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
-        
+
         for post in posts:
-            print(f"ID: {post['id']}, Title: {post['title']}, Body: {post['body']}")
+            print(
+                f"ID: {post['id']}, Title: {post['title']}, "
+                f"Body: {post['body']}"
+            )
+
 
 fetch_and_print_posts()
 
+
 def fetch_and_save_posts():
-     
-     if response.status_code == 200:
-        
+    """Fetch posts from the API and save them to a CSV file."""
+    if response.status_code == 200:
         posts = response.json()
-        
-        data_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
-        
+
+        data_posts = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+
         for post in posts:
             print(post.get('body'))
-            
+
         with open("posts.csv", "w", newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file)
+
             writer.writerow(['id', 'title', 'body'])
-            
+
             for post in data_posts:
                 writer.writerow([post['id'], post['title'], post['body']])
-            
+
 
 fetch_and_save_posts()
