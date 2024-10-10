b4ce6b8581315ae7dcb51a73e4524663ca79171a
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-10 15:51:44 +0200

    try to check task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+PORT = 8000
+
+
+class Server(BaseHTTPRequestHandler):
+    """Simple HTTP Server that handles GET requests."""
+
+    def do_GET(self):
+        """Handle GET requests."""
+        if self.path == "/":
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == "/data":
+
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+                }
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/info":
+
+            info = {
+                "version": "1.0",
+                "description":
+                "A simple API built with http.server"
+                }
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+with HTTPServer(("", PORT), Server) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
