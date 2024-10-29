29e435897e8760899aab8432cc173132257e6c7a
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-29 14:34:08 +0100

    add group by

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -19,4 +19,5 @@ CREATE TABLE IF NOT EXISTS tv_show_genres (
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_shows.title, tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -18,4 +18,5 @@ CREATE TABLE IF NOT EXISTS tv_show_genres (
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_shows.title, tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
