b64505b22779e9bccff0dc11de0f74659d3e16b2
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-30 11:59:18 +0100

    add task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" 
+"""
 script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
@@ -17,7 +17,8 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+        ORDER BY id ASC".format(sys.argv[4]))
     states = cursor.fetchall()
     for state in states:
         print(state)
