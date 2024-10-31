30c2a47883b9ef6a1ba604aae6423399c40a877b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-31 09:25:21 +0100

    add import

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -2,6 +2,7 @@
 """Start link class to table in database
 """
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
