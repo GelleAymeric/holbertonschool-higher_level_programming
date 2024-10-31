06698dca987cf79c10d6d9e423baefea5d71d85e
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-31 09:47:33 +0100

    fix pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 class State(Base):
     """Class definition for State."""
     __tablename__ = 'states'
-    
+
     id = Column(Integer,
                 primary_key=True,
                 autoincrement=True,
