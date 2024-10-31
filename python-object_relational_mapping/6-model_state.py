a6441ce0a6ea6970ea3de67f7018bd5ac7e02b5d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-31 09:24:31 +0100

    fix ptycodestyle

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class representing the 'states' table"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                unique=True,
+                nullable=False
+                )
     name = Column(String(128), nullable=False)
