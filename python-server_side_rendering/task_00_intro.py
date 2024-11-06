ae146fa84cb47dbb49985750fc7d57903b41824d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-06 10:36:28 +0100

    add files and check task 0

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,35 @@
+import os
+
+def generate_invitations(template, attendees):
+
+    if not isinstance(template, str):
+        raise ValueError("Template must be a string")
+
+    if not isinstance(attendees, list) or not all(isinstance(item, dict) for item in attendees):
+        raise ValueError("Attendees must be a list")
+
+    if template.strip() == "":
+        raise ValueError("Template is empty")
+
+    if not attendees:
+        raise ValueError("Attendees list is empty")
+
+    for attendee in attendees:
+        if not all(key in attendee for key in \
+            ["name", "event_title", "event_date", "event_location"]):
+            raise ValueError("Attendee must have keys")
+
+    for index, attendee in enumerate(attendees, start=1):
+
+        content = template.format(
+            name=attendee.get("name", "N/A"),
+            event_title=attendee.get("event_title", "N/A"),
+            event_date=attendee.get("event_date", "N/A"),
+            event_location=attendee.get("event_location", "N/A")
+        )
+
+        file_name = f"output_{index}.txt"
+        with open(file_name, "w") as file:
+            file.write(content)
+
+        print(f"Invitation generated: {file_name}")
